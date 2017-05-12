d94ae8449edb31aac3b66e4e6a7324e0cb59b799
Winson <laxsong@hotmail.com>
2017-05-12 23:30:55 +0800

    static html page

--- src/main/java/com/yquants/restfulws/client/web/RestfulServletClient.java
+++ src/main/java/com/yquants/restfulws/client/web/RestfulServletClient.java
@@ -1,8 +1,13 @@
 package com.yquants.restfulws.client.web;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 /**
  * Created by Wei on 2017/5/12.
@@ -13,8 +18,16 @@ public class RestfulServletClient {
 
     @RequestMapping("/index")
     public String index() {
-
         return "hello";
+    }
+
+    @RequestMapping("/twitter")
+    public ResponseEntity<String> twitter() throws Exception{
+        URI location = new URI("abc");
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(location);
+        responseHeaders.set("MyResponseHeader", "MyValue");
+        return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
     }
 
 }
